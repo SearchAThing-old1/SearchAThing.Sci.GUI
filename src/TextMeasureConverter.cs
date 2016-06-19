979d8f717479f676bd98e1538f518cfa4a81946b
Lorenzo Delana <oss.devel@searchathing.com>
2016-06-19 19:40:02 +0200

    text measure converter to bind Measure through a simple TextBox

---
+++ src/TextMeasureConverter.cs
@@ -0,0 +1,66 @@
+﻿#region SearchAThing.Sci.GUI, Copyright(C) 2016 Lorenzo Delana, License under MIT
+/*
+* The MIT License(MIT)
+* Copyright(c) 2016 Lorenzo Delana, https://searchathing.com
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*/
+#endregion
+
+using System;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace SearchAThing.Sci.GUI
+{
+
+    public class MeasureTextConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // targetType : string
+
+            if (value == null) return null;
+
+            var measure = (Measure)value;
+
+            return measure.ToString(culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // targetType : Measure
+
+            if (value == null) return null;
+
+            var text = (string)value;
+
+            var res = Measure.TryParse(text, PQCollection.Length, culture);
+
+            if (res == null) return DependencyProperty.UnsetValue;
+
+            return res;
+        }
+
+    }
+
+}
